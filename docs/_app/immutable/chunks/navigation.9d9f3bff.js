0eead019cc7f0c9ac5a3e7cfb11a9de75b159156
dheerajdhall27 <dheerajdhall2709@gmail.com>
2023-09-27 21:07:26 -0400

    Build to fix css issue

---
+++ docs/_app/immutable/chunks/navigation.9d9f3bff.js
@@ -0,0 +1,1 @@
+import{j as o}from"./singletons.60cf02ff.js";const e=o("goto");export{e as g};
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -5,26 +5,3 @@
 <div class="blog-main-page-container">
 	<slot />
 </div>
-
-<style>
-	.blog-main-header-container {
-		background-color: #77c3ec;
-		height: 200px;
-	}
-
-	.blog-main-heading {
-		text-align: center;
-		font-size: 40px;
-		font-family: 'Roboto', sans-serif;
-		padding-top: 50px;
-		color: white;
-	}
-
-	.blog-owner-name {
-		text-align: center;
-		font-size: 20px;
-		font-family: 'Roboto', sans-serif;
-		margin-top: 10px;
-		color: white;
-	}
-</style>
