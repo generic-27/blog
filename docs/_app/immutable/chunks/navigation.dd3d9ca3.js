e52508572c6094dde37b7871dd18ca88e47e8967
dheerajdhall27 <dheerajdhall2709@gmail.com>
2023-09-27 21:50:55 -0400

    Added trailing slash true

---
+++ docs/_app/immutable/chunks/navigation.dd3d9ca3.js
@@ -0,0 +1,1 @@
+import{j as o}from"./singletons.af76905c.js";const e=o("goto");export{e as g};
--- src/components/blog/main_header/article-main-header.svelte
+++ src/components/blog/main_header/article-main-header.svelte
@@ -2,7 +2,7 @@
 	import { goto } from '$app/navigation';
 
 	function handleHomeClick() {
-		goto('/');
+		goto('/blog/');
 	}
 </script>
 
